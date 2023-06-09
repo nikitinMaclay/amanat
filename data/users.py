b25b4dfebe0ab5174bb8dbcfaba2ffbf72de9b0c
Михаил <guicokiuytr@gmail.com>
2023-06-09 20:40:40 +0600

    Initial commit

---
+++ data/users.py
@@ -0,0 +1,26 @@
+import datetime
+import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy import orm
+from sqlalchemy.orm import relationship
+from sqlalchemy_serializer import SerializerMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    phone_number = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    password = sqlalchemy.Column(sqlalchemy.String)
+    user_city = sqlalchemy.Column(sqlalchemy.String)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
---
+++ main.py
@@ -0,0 +1,67 @@
+import datetime
+
+from flask import Flask, render_template, redirect, request, make_response, abort, jsonify
+
+from data import db_session
+from data.users import User
+
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'amanat_copy'
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    days=365
+)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
+def index():
+    print(1)
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    pass
+
+
+@app.route('/user_profile', methods=["GET", "POST"])
+@login_required
+def user_profile():
+    if request.method == "GET":
+        pass
+    else:
+        abort(404)
+    return render_template('cabinet.html')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.errorhandler(400)
+def bad_request(_):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
+
+
+def main():
+    db_session.global_init("databases/amanat.db")
+
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
